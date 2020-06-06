269ced496f466f9688f6b9b442983622f6acf45f
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2020-06-06 20:29:20 +0900

    ハンズオンの流れ描いた

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,121 @@
 <template>
-  <v-responsive
-    class="overflow-y-auto"
-    max-height="400"
-  >
-    <div class="pa-6 text-center">
-      This is a home page
-    </div>
-  </v-responsive>
+  <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
+    <v-timeline-item
+      v-for="(timeline, i) in timelines"
+      :key="i"
+      :color="timeline.color"
+      :icon="timeline.icon"
+      fill-dot
+    >
+      <v-card
+        :color="timeline.color"
+        dark
+      >
+        <v-card-title class="title">{{timeline.title}}</v-card-title>
+        <v-card-text class="white text--primary">
+          <p>{{timeline.description}}</p>
+          <v-btn
+            v-if="timeline.link"
+            :color="timeline.color"
+            :href="timeline.link"
+            class="mx-0"
+            outlined
+          >
+            {{timeline.linkText}}
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
 </template>
 
 <script>
 export default {
   name: 'Home',
   components: {},
+  data: () => ({
+    timelines: [
+      {
+        color: 'green lighten-1',
+        title: '事前準備',
+        description: 'ハンズオン前にnodeやyarn環境を整えましょう',
+        icon: 'mdi-home',
+        link: 'https://github.com/arakawamoriyuki/vue-nuxt-handson/readme.md',
+        linkText: 'github readme',
+      },
+      {
+        color: 'green lighten-1',
+        title: '座学',
+        description: 'xxxx',
+        icon: 'mdi-book-open-variant',
+        link: 'https://github.com/arakawamoriyuki/vue-nuxt-handson/readme.md',
+        linkText: 'github readme',
+      },
+      {
+        color: 'green lighten-1',
+        title: 'Web基礎',
+        description: 'xxxx',
+        icon: 'mdi-book-open-variant',
+        link: 'https://github.com/arakawamoriyuki/vue-nuxt-handson/readme.md',
+        linkText: 'github readme',
+      },
+      {
+        color: 'green lighten-1',
+        title: 'Vue.jsについて',
+        description: 'xxxx',
+        icon: 'mdi-book-open-variant',
+        link: 'https://github.com/arakawamoriyuki/vue-nuxt-handson/readme.md',
+        linkText: 'github readme',
+      },
+      {
+        color: 'green lighten-1',
+        title: '環境構築',
+        description: 'xxxx',
+        icon: 'mdi-laptop',
+        link: 'https://github.com/arakawamoriyuki/vue-nuxt-handson/tree/setup',
+        linkText: 'github setup tag',
+      },
+      {
+        color: 'blue lighten-1',
+        title: 'カウンター',
+        description: 'xxxx',
+        icon: 'mdi-laptop',
+        link: 'https://github.com/arakawamoriyuki/vue-nuxt-handson/tree/counter',
+        linkText: 'github counter tag',
+      },
+      {
+        color: 'blue lighten-1',
+        title: 'TODOリスト',
+        description: 'xxxx',
+        icon: 'mdi-laptop',
+        link: 'https://github.com/arakawamoriyuki/vue-nuxt-handson/tree/todolist',
+        linkText: 'github todolist tag',
+      },
+      {
+        color: 'blue lighten-1',
+        title: 'プロフィール',
+        description: 'xxxx',
+        icon: 'mdi-laptop',
+        link: 'https://github.com/arakawamoriyuki/vue-nuxt-handson/tree/profile',
+        linkText: 'github profile tag',
+      },
+      {
+        color: 'blue lighten-1',
+        title: '静的サイトホスティング',
+        description: 'xxxx',
+        icon: 'mdi-laptop',
+        link: 'https://github.com/arakawamoriyuki/vue-nuxt-handson/readme.md',
+        linkText: 'github readme',
+      },
+      {
+        color: 'blue lighten-1',
+        title: 'nuxt (optional)',
+        description: 'xxxx',
+        icon: 'mdi-laptop',
+        link: 'https://github.com/arakawamoriyuki/vue-nuxt-handson/tree/nuxt',
+        linkText: 'github nuxt tag',
+      },
+    ],
+  }),
 };
 </script>
