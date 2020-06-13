2418264d9ab70153d9bcb1fd9746c5ae94c57801
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2020-06-13 15:39:53 +0900

    before eachで遷移しよう

--- tests/e2e/specs/index.js
+++ tests/e2e/specs/index.js
@@ -1,8 +1,9 @@
-// https://docs.cypress.io/api/introduction/api.html
-
 describe('/', () => {
-  it('事前準備が表示される', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  it('事前準備が表示される', () => {
     cy.contains('.v-timeline .v-timeline-item .v-card .title', '事前準備');
   });
 });
