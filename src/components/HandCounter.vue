21e8a11f2dc87be451ac2670e7c06df572ac3927
Moriyuki Arakawa <arakawamoriyuki@gmail.com>
2020-06-13 13:45:55 +0900

    カウンター実装

---
+++ src/components/HandCounter.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-responsive
+    class="overflow-y-auto"
+    max-height="400"
+  >
+    <p>{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+  </v-responsive>
+</template>
+
+<script>
+import store from '@/store/HandCounterStore';
+
+export default {
+  name: 'hand-counter',
+  components: {},
+  computed: {
+    count() {
+      return store.state.count;
+    },
+  },
+  methods: {
+    increment() {
+      store.commit('increment');
+    },
+    decrement() {
+      store.commit('decrement');
+    },
+  },
+};
+</script>
--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -6,11 +6,17 @@
     <div class="pa-6 text-center">
       This is a counter page
     </div>
+    <hand-counter/>
   </v-responsive>
 </template>
 
 <script>
+import HandCounter from '@/components/HandCounter.vue';
+
 export default {
   name: 'counter',
+  components: {
+    HandCounter,
+  },
 };
 </script>
